d417c7e2ad41f164b6d76d6b089fcdfbe458af2f
psailla2 <pierric.saillard@edu.univ-fcomte.fr>
2024-12-16 11:04:05 +0100

    Added border and group hit selection function

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -9,6 +9,15 @@ func play(sound,where=Vector2(0,0)):
 func playPool(soundA:Array[AudioStream],where=Vector2(0,0)):
 	play(soundA.pick_random(),where)
 
+# Returns a new group of targeted entities  
+func groupGetHit(group:Array[Node2D],where:Vector2,ray:float)->Array[Node2D]:
+	var bonked:Array[Node2D]=[]
+	ray*=ray
+	for e in group:
+		if e.global_position.distance_squared_to(where)<=ray:
+			bonked.append(e)
+	return bonked
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
