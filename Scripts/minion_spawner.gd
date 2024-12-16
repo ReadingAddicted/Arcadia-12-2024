235869759ace6d64200514fc9068787151381217
ReadingAddicted <lecoupanech@gmail.com>
2024-12-16 18:31:58 +0100

    Correction sprite and max amount for the spawner

--- Scripts/minion_spawner.gd
+++ Scripts/minion_spawner.gd
@@ -1,8 +1,16 @@
 extends StaticBody2D
 
+@export var team: bool = true
 @onready var minion = preload("res://Scenes/minion.tscn")
+@export var max_minions: int = 5
+var amount_minions: int = 0
+
 
 func _on_timer_timeout() -> void:
-	var child = minion.instantiate()
-	child.position = $SpawningPoint.global_position
-	get_parent().add_child(child)
+	if(amount_minions < max_minions):
+		var child = minion.instantiate()
+		child.team = team
+		child.creator = self
+		child.position = $SpawningPoint.global_position
+		get_parent().add_child(child)
+		amount_minions += 1
